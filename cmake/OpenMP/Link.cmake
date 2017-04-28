54c05e0c708a25cd2ee4d4e15158848a6c78cf2b
Soeren Peters - iRMB <peters.soeren@gmx.net>
2017-04-28 15:21:51 +0200

    - Bugfix: openMP flag

--- cmake/OpenMP/Link.cmake
+++ cmake/OpenMP/Link.cmake
@@ -2,8 +2,8 @@ macro(linkOpenMP targetName)
 
 find_package(OpenMP)
 if (OPENMP_FOUND)
-	set (CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${OPENMP_C_FLAGS}")
-	set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${OPENMP_CXX_FLAGS}")
+	set (CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${OpenMP_C_FLAGS}")
+	set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${OpenMP_CXX_FLAGS}")
 endif()
 
 endmacro(linkOpenMP)
