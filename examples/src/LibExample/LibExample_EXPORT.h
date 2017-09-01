c4670bb04067229e95ce09b798cc13d26376ed7d
Soeren Peters <peters.soeren@gmx.net>
2017-09-02 00:05:46 +0200

    - stop tracking from export header

--- examples/src/LibExample/LibExample_EXPORT.h
+++ examples/src/LibExample/LibExample_EXPORT.h
@@ -9,20 +9,20 @@
 #  ifndef LibExample_EXPORT
 #    ifdef LibExample_EXPORTS
         /* We are building this library */
-#      define LibExample_EXPORT __declspec(dllexport)
+#      define LibExample_EXPORT __attribute__((visibility("default")))
 #    else
         /* We are using this library */
-#      define LibExample_EXPORT __declspec(dllimport)
+#      define LibExample_EXPORT __attribute__((visibility("default")))
 #    endif
 #  endif
 
 #  ifndef LIBEXAMPLE_NO_EXPORT
-#    define LIBEXAMPLE_NO_EXPORT 
+#    define LIBEXAMPLE_NO_EXPORT __attribute__((visibility("hidden")))
 #  endif
 #endif
 
 #ifndef LIBEXAMPLE_DEPRECATED
-#  define LIBEXAMPLE_DEPRECATED __declspec(deprecated)
+#  define LIBEXAMPLE_DEPRECATED __attribute__ ((__deprecated__))
 #endif
 
 #ifndef LIBEXAMPLE_DEPRECATED_EXPORT
@@ -33,9 +33,10 @@
 #  define LIBEXAMPLE_DEPRECATED_NO_EXPORT LIBEXAMPLE_NO_EXPORT LIBEXAMPLE_DEPRECATED
 #endif
 
-#define DEFINE_NO_DEPRECATED 0
-#if DEFINE_NO_DEPRECATED
-# define LIBEXAMPLE_NO_DEPRECATED
+#if 0 /* DEFINE_NO_DEPRECATED */
+#  ifndef LIBEXAMPLE_NO_DEPRECATED
+#    define LIBEXAMPLE_NO_DEPRECATED
+#  endif
 #endif
 
 #endif
