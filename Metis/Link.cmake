3aec1f1d52a1a548dad9e2abf4901bdd72cc2393
Soeren Peters <peters.soeren@gmx.net>
2017-09-04 21:36:43 +0200

    Squashed 'CMakeMacros/' changes from 52ffd53..c69c90c
    c69c90c - add readme
    b6f356e - introduce variable for the cmake folder
    e1e8877 - add machinefile for environment paths -> environment.cmake will detect the computer name. A matching file with the computer name is required.
    
    git-subtree-dir: CMakeMacros
    git-subtree-split: c69c90cb08ec107412401bb5ed2a7cec9d15d64e

--- Metis/Link.cmake
+++ Metis/Link.cmake
@@ -1,6 +1,6 @@
 macro(linkMetis targetName)
 
-	include(${CMAKE_SOURCE_DIR}/cmake/Metis/FindMetis.cmake)
+	include(${CMAKE_SOURCE_DIR}/${cmakeMacroPath}/Metis/FindMetis.cmake)
 	include_directories(${METIS_INCLUDE_DIRS})
 
 	target_link_libraries(${targetName} ${METIS_LIBRARIES})
