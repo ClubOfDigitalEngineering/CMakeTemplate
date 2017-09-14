d2f11c1d8af79f370436c58223cfbd821d575693
Soeren Peters <peters@irmb.tu-bs.de>
2017-09-14 15:28:30 +0200

    Squashed 'CMakeMacros/' changes from c69c90c..a9f66eb
    a9f66eb - changes structure to use CMakeMacros subtree - deletes original cmake macros from project - adds MacOSXBundle macro to CMakeMacros - moves MachineFiles folder in Environment folder - ignores MachineFiles folder in Environment folder
    553003b - boost function with components as parameters - vtk include_directories(${VTK_INCLUDE_DIRS})
    
    git-subtree-dir: CMakeMacros
    git-subtree-split: a9f66eb5e75221e84d5f9fd884056310b97a37c4

--- Boost/Link.cmake
+++ Boost/Link.cmake
@@ -1,8 +1,29 @@
-macro(linkBoost targetName)
 
-	FIND_PACKAGE( Boost REQUIRED COMPONENTS signals)
+# usage: linkBoost(${targetName} COMPONENTS arg0 arg1 arg2 ...)
+# e.g.   linkBoost(${targetName} COMPONENTS "Thread" "Signals")
+# e.g.   linkBoost(${targetName})
+
+function(linkBoost)
+  if (WIN32)
+	add_definitions( -DBOOST_ALL_NO_LIB )
+	add_definitions( -DBOOST_ALL_DYN_LINK )
+  endif()
+
+  list(LENGTH ARGV arguments)
+  math(EXPR argn "${arguments} - 1")
+
+  if(argn EQUAL 0)
+    FIND_PACKAGE( Boost REQUIRED)
 	INCLUDE_DIRECTORIES( ${Boost_INCLUDE_DIR} )
-
 	TARGET_LINK_LIBRARIES( ${targetName} ${Boost_LIBRARIES} )
+    return()
+  endif()
 
-endmacro(linkBoost)
+  foreach(index RANGE 2 ${argn})
+    list(GET ARGV ${index} component)
+	FIND_PACKAGE( Boost REQUIRED COMPONENTS ${component})
+	INCLUDE_DIRECTORIES( ${Boost_INCLUDE_DIR} )
+	TARGET_LINK_LIBRARIES( ${targetName} ${Boost_LIBRARIES} )
+  endforeach()
+  
+endfunction(linkBoost)
