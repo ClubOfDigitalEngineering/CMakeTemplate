f25525f769ba4e893e334e115cdeb105c09b7a55
Soeren Peters <peters@irmb.tu-bs.de>
2017-09-28 15:20:02 +0200

    - adds Sharp example - CSharp requires cmake version 3.9

---
+++ examples/applications/CSharpExample/main.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace CSharpExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
