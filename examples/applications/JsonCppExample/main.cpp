dc6f53c106e49f603a4fb68f2dd7b732590cd786
Soeren Peters <peters.soeren@gmx.net>
2017-11-19 13:09:31 +0100

    - set json file path in cmake - using CharReader due to Json::Reader is deprecated

--- examples/applications/JsonCppExample/main.cpp
+++ examples/applications/JsonCppExample/main.cpp
@@ -1,24 +1,51 @@
 //example from https://en.wikibooks.org/wiki/JsonCpp
+//modified due to the class Json::Reader is deprecated
 
 #include <iostream>
 #include <fstream>
 #include <json/json.h>
 
-using namespace std;
+std::string getStringFromStream(std::ifstream &stream)
+{
+    return std::string((std::istreambuf_iterator<char>(stream)),
+                         std::istreambuf_iterator<char>());
+}
 
-int main() {
-	//TODO: Add path to alice.json
-	std::string path = "C:/Users/Soeren/Documents/Templates/CMakeTemplate/examples/applications/JsonCppExample/alice.json";
-	ifstream ifs(path);
-	Json::Reader reader;
-	Json::Value obj;
-	reader.parse(ifs, obj); // reader can also read strings
-	cout << "Book: " << obj["book"].asString() << endl;
-	cout << "Year: " << obj["year"].asUInt() << endl;
-	const Json::Value& characters = obj["characters"]; // array of characters
-	for (int i = 0; i < characters.size(); i++) {
-		cout << "    name: " << characters[i]["name"].asString();
-		cout << " chapter: " << characters[i]["chapter"].asUInt();
-		cout << endl;
-	}
+const char* getPointerFromString(const std::string &string)
+{
+    return string.c_str();
+}
+
+void print(Json::Value jsonValues)
+{
+    std::cout << "Book: " << jsonValues["book"].asString() << std::endl;
+    std::cout << "Year: " << jsonValues["year"].asUInt()   << std::endl;
+    const Json::Value& characters = jsonValues["characters"];
+    for (int i = 0; i < characters.size(); i++) {
+        std::cout << "    name: " << characters[i]["name"].asString();
+        std::cout << " chapter: " << characters[i]["chapter"].asUInt();
+        std::cout << std::endl;
+    }
+}
+
+int main()
+{
+    std::string path = JSONPATH;
+    std::string jsonFilePath = path + "/alice.json";
+    std::ifstream jsonStream = std::ifstream(jsonFilePath);
+
+    std::string jsonFile = getStringFromStream(jsonStream);
+    const char* beginDoc = getPointerFromString(jsonFile);
+    const char* endDoc = beginDoc + jsonFile.size();
+
+    Json::CharReader* reader = Json::CharReaderBuilder().newCharReader();
+
+    Json::Value jsonValues;
+    std::string errors;
+
+	reader->parse(beginDoc, endDoc, &jsonValues, &errors);
+
+    print(jsonValues);
+
+    delete reader;
 }
